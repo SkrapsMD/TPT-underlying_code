2c1bfea73b6c4cd8dcd90509103c5d0852c5cf04
SkrapsMD <michael.sparks@atl.frb.org>
2025-07-22 09:54:28 -0400

    Added some mappings from the BEA folks to the end-use codes and the HS codes.

---
+++ HS to BEA Data/code/Alternative Census Mapping/01_Read_in_census_mappings.py
@@ -0,0 +1,6 @@
+# From BEA's ITA, they gave us this mapping of 
+# HS codes to different things like naics. https://www.census.gov/foreign-trade/reference/index.html
+# These codes shoudl map  to the appropriate naics codes, as well as these BEA End-use Codes... 
+
+# to use these we need to use read in the fixed use files and maintain the BEA end use connection. 
+
